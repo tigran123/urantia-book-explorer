f2244e775896c0de5887f64e101c3ea41c260c4d
Tigran Aivazian <aivazian.tigran@gmail.com>
2017-03-30 18:52:05 +0100

    Slight optimisation

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 var active_column = 'col1';
-var colpaper_map = { 'col1': '000', 'col2': '000', 'col3': '000' };
+var colpaper_map = {'col1': 0, 'col2': 0, 'col3': 0};
 
 $('.buttons').button();
-$('#tabs').tabs();
-$('#tabs').on('click', 'a', function(e) {
+$('#tabs').tabs().on('click', 'a', function(e) {
    switch(e.target.id) {
       case 'home':
          $('#search_text').focus();
@@ -45,12 +44,11 @@ $('.colmod').selectmenu({
      var col = $(this).attr('id').replace('mod', '');
      var mod_idx = ui.item.value;
      var paper = colpaper_map[col];
-     var paper_num = paper.replace(/0*(..*)/,'$1'); /* strip the leading zeros */
-     $('#' + col + 'txt').load('text/' + mod_idx + '/p' + paper + '.html');
+     $('#' + col + 'txt').load('text/' + mod_idx + '/p' + ("000" + paper).slice(-3) + '.html');
      $('#' + col + 'toc').load('text/' + mod_idx + '/toc.html', function() {
         var toc = $(this).find('.toc');
         toc.bonsai();
-        $('#' + col + 'title').html(toc.find('.U' + paper_num + '_0_1').html());
+        $('#' + col + 'title').html(toc.find('.U' + paper + '_0_1').html());
         document.cookie = col + 'mod=' + mod_idx + '; expires=Fri, 31 Dec 9999 23:59:59 GMT';
      });
   },
@@ -76,13 +74,12 @@ $('.coltxt').each(function() {
    var toc_id = '#' + col + 'toc';
    var mod_idx = $('#' + col + 'mod').val();
    var paper = colpaper_map[col];
-   var paper_num = paper.replace(/0*(..*)/,'$1'); /* strip the leading zeros */
-   $(this).load('text/' + mod_idx + '/p' + paper + '.html');
+   $(this).load('text/' + mod_idx + '/p' + ("000" + paper).slice(-3) + '.html');
    if (col == active_column) $('#notes').load('text/' + mod_idx + '/notes.html');
    $(toc_id).load('text/' + mod_idx + '/toc.html', function() {
       var toc = $(this).find('.toc');
       toc.bonsai();
-      $('#' + col + 'title').html(toc.find('.U' + paper_num + '_0_1').html());
+      $('#' + col + 'title').html(toc.find('.U' + paper + '_0_1').html());
       if (active_column == col) $(toc_id).removeClass('hidden');
       /* XXX: the following code is executed three times, but if we use deferred functions we could run it only once */
       if (!$('#tooltips').is(':checked')) { $(document).tooltip('option', 'disabled', true); }
@@ -122,15 +119,14 @@ $('.toc_container,#search_results,#notes').on('click', 'a', function(e) {
   e.preventDefault();
   var delay = $('#animations').is(':checked') ? 606 : 0;
   var href = $(this).attr('href');
-  var paper_num = href.replace(/.U([0-9][0-9]*)_.*_.*/,'$1');
-  var paper = ("000" + paper_num).slice(-3);
+  var paper = href.replace(/.U([0-9][0-9]*)_.*_.*/,'$1');
   var $coltxt = $('#' + active_column + 'txt');
   var $marks = $(this).parent().find('mark');
   var mark_opts = {"accuracy": "exact", "separateWordSearch": false, "acrossElements": true};
   if (colpaper_map[active_column] != paper) { /* need to load a different paper */
      var mod_idx = $('#' + active_column + 'mod').val();
-     $coltxt.load('text/' + mod_idx + '/p' + paper + '.html', function() {
-        var title = $('#' + active_column + 'toc').find('.toc').find('.U' + paper_num + '_0_1').html();
+     $coltxt.load('text/' + mod_idx + '/p' + ("000" + paper).slice(-3) + '.html', function() {
+        var title = $('#' + active_column + 'toc').find('.toc').find('.U' + paper + '_0_1').html();
         $('#' + active_column + 'title').html(title);
         colpaper_map[active_column] = paper;
         $coltxt.scrollTo(href, delay);
@@ -154,15 +150,14 @@ $('.coltxt').on('click', 'a', function(e) {
   var this_column = e.delegateTarget;
   var delay = $('#animations').is(':checked') ? 606 : 0;
   var href = $(this).attr('href');
-  var paper_num = href.replace(/.U([0-9][0-9]*)_.*_.*/,'$1');
-  var paper = ("000" + paper_num).slice(-3);
+  var paper = href.replace(/.U([0-9][0-9]*)_.*_.*/,'$1');
   $('.coltxt').not(this_column).each(function() {
       var col = $(this).attr('id').replace('txt','');
       var mod_idx = $('#' + col + 'mod').val();
       var $coltxt = $('#' + col + 'txt');
       if (colpaper_map[col] != paper) { /* need to load a different paper */
-         $coltxt.load('text/' + mod_idx + '/p' + paper + '.html', function() {
-             var title = $('#' + col + 'toc').find('.toc').find('.U' + paper_num + '_0_1').html();
+         $coltxt.load('text/' + mod_idx + '/p' + ("000" + paper).slice(-3) + '.html', function() {
+             var title = $('#' + col + 'toc').find('.toc').find('.U' + paper + '_0_1').html();
              $('#' + col + 'title').html(title);
              colpaper_map[col] = paper;
              $coltxt.scrollTo(href, delay);
