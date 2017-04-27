eee79a01793ec4202a30311ba6f3990cf76dcecb
alyuev <alyuev@mail.ru>
2017-04-27 22:02:01 +0300

    Небольшой рефакторинг кода

--- search.php
+++ search.php
@@ -1,4 +1,32 @@
 <?php
+//первоначальная установка переменных
+function init_vars($search_part) {
+   $ret_vars = [];
+   switch ($search_part) {
+      case 0:
+         $ret_vars['i_min'] = 0;
+         $ret_vars['i_max'] = 196;
+         break;
+      case 1:
+         $ret_vars['i_min'] = 0;
+         $ret_vars['i_max'] = 31;
+         break;
+      case 2:
+         $ret_vars['i_min'] = 32;
+         $ret_vars['i_max'] = 56;
+         break;
+      case 3:
+         $ret_vars['i_min'] = 57;
+         $ret_vars['i_max'] = 119;
+         break;
+      case 4:
+         $ret_vars['i_min'] = 120;
+         $ret_vars['i_max'] = 196;
+         break;
+   }
+   return $ret_vars;
+}
+
 //завершаем скрипт
 function end_search($par_count, $match_count, $matches) {
    $json = ['par_count' => $par_count, 'match_count' => $match_count, 'matches' => $matches];
