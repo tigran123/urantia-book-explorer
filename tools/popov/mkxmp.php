79190ff17f1f603e179d6e9059f99be679010fae
Tigran Aivazian <aivazian.tigran@gmail.com>
2017-05-22 16:25:10 +0100

    Convert some tags

--- tools/popov/mkxmp.php
+++ tools/popov/mkxmp.php
@@ -2,6 +2,12 @@
 ini_set('memory_limit','300M');
 system("rm -rf exemplars ; mkdir -p exemplars/21");
 
+$textp = '/^(?:<\/span>)?(?:<span style="color: #000000;">)?((?:<i>)?[\d«а-яА-Я[].*?)(?:<span .*?)?(?:<br\/>)?(?:<\/p>)?$/u';
+
+$headp1 = '/^<h5 style="text-align: left;"><span id="[^"]*">(?:<b>|<strong>)(.*)<span class="" style="display:block;clear:both;height: 0px;padding-top: 50px;border-top-width:0px;border-bottom-width:0px;"><\/span>(<\/i>)?(?:<\/b>|<\/strong>)<\/span><\/h5>/u';
+
+$headp2 = '/^<h5[^>]*><span[^>]*>(?:<b>|<strong>)(.*) *<\/b><\/span><\/h5>/u';
+
 for ($i = 0; $i <= 196; $i++) {
    $filename = sprintf("html/p%03d.html", $i);
    $ofilename = sprintf("exemplars/21/p%03d.txt", $i);
@@ -10,17 +16,21 @@ for ($i = 0; $i <= 196; $i++) {
    if ($lines == FALSE) continue;
    $linenum = 1;
    foreach ($lines as $line) {
-      if (preg_match('/^(?:<\/span>)?(?:<span style="color: #000000;">)?((?:<i>)?[\d«а-яА-Я[].*?)(?:<span .*?)?(?:<br\/>)?(?:<\/p>)?$/u', $line, $matches)) {
-         $olines[] = '['.$linenum.'] '.$matches[1].PHP_EOL;
-         $linenum++;
-      } elseif (preg_match('/^<h5 style="text-align: left;"><span id="[^"]*">(?:<b>|<strong>)(.*)<span class="" style="display:block;clear:both;height: 0px;padding-top: 50px;border-top-width:0px;border-bottom-width:0px;"><\/span>(<\/i>)?(?:<\/b>|<\/strong>)<\/span><\/h5>/u', $line, $matches)) {
+      if (preg_match($textp, $line, $matches) || preg_match($headp1, $line, $matches) || preg_match($headp2, $line, $matches)) {
+         $clean = convert_tags($matches[1]);
          if (isset($matches[2]))
-            $olines[] = '['.$linenum.'] '.$matches[1].$matches[2].PHP_EOL;
+            $olines[] = '['.$linenum.'] '.$clean.$matches[2].PHP_EOL;
          else
-            $olines[] = '['.$linenum.'] '.$matches[1].PHP_EOL;
+            $olines[] = '['.$linenum.'] '.$clean.PHP_EOL;
          $linenum++;
       }
    }
    file_put_contents($ofilename, $olines);
+}
+
+function convert_tags($text) {
+   $search = ['<em>', '</em>'];
+   $replace = ['<i>', '</i>'];
+   return str_replace($search, $replace, $text);
 }
 ?>
