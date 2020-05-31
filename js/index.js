2005b00e2a4d77ae1e5017986e8b7d97f28c1dff
Tigran Aivazian <aivazian.tigran@gmail.com>
2020-05-31 21:17:36 +0100

    Handle Ctrl-X in the case when nothing is selected Place the focus in the edit box when Ctrl-X is pressed

--- js/index.js
+++ js/index.js
@@ -459,14 +459,18 @@ $(document).keydown(function(event) {
    } else if (ctrl && key == 88) { /* Ctrl + X */
       event.preventDefault();
       seltext = window.getSelection().toString();
-      $('#search_text').val(seltext);
-      var saved_active_column = active_column;
-      active_column = selection_column;
-      var saved_search_mode = $('#search_mode').val();
-      $('#search_mode').val('exact').selectmenu('refresh');
-      $('#search').click();
-      active_column = saved_active_column;
-      $('#search_mode').val(saved_search_mode).selectmenu('refresh');
+      if (seltext === '')
+         $('#clear').click();
+      else {
+         $('#search_text').val(seltext);
+         var saved_active_column = active_column;
+         active_column = selection_column;
+         var saved_search_mode = $('#search_mode').val();
+         $('#search_mode').val('exact').selectmenu('refresh');
+         $('#search').click();
+         active_column = saved_active_column;
+         $('#search_mode').val(saved_search_mode).selectmenu('refresh');
+      }
    } else if (ctrl && key == 79) { /* Ctrl + O */
       event.preventDefault();
       $('#toc_expand_collapse').click();
